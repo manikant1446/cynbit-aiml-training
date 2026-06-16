33f75cbc50c74c5ca48403365fb353e29a01a35b
Manikant Kumar <manikant1446@gmail.com>
2026-06-16 15:04:51 +0530

    Day 5 Task

---
+++ GradeChecker.py
@@ -0,0 +1,9 @@
+percentage=int(input("Enter your percentage: "))
+if percentage >= 60:
+    print("Grade : A")
+elif percentage >= 45:
+    print("Grade : B")
+elif percentage >= 30:  
+    print("Grade : C")
+else:
+    print("Grade : Fail")
